2081c7fd5941f4b6eb89aaeb7554141843a5d085
NgocTam <ngoctam3096@gmail.com>
2017-10-10 16:40:24 +0700

    View detail course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,18 @@
 class CoursesController < ApplicationController
+  before_action :load_course, only: %i(show)
+
   def index
     @courses = Course.paginate(page: params[:page], per_page: Settings.per_page.config)
+  end
+
+  def show; end
+
+  private
+
+  def load_course
+    @course = Course.find_by id: params[:id]
+    return if @course
+    flash[:danger] = t "controllers.courses.flash_danger"
+    redirect_to :root
   end
 end
