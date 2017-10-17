91ef1bc8637a4046e97797c4be2f14b55135e259
Your Name <you@example.com>
2017-10-17 22:07:15 +0700

    suppervisor_subject_index_show

--- app/controllers/suppervisor/subjects_controller.rb
+++ app/controllers/suppervisor/subjects_controller.rb
@@ -2,7 +2,50 @@ class Suppervisor::SubjectsController < ApplicationController
   layout "suppervisor_layout"
   before_action :logged_in_user
   before_action :verify_suppervisor
+  before_action :load_subject, only: %i(show destroy)
+
   def index
     @subjects = Subject.paginate(page: params[:page], per_page: Settings.per_page.config)
+  end
+
+  def show
+    load_courses @subject
+    load_trainers_trainees @subject
+    load_tasks @subject
+  end
+
+  def new; end
+
+  def create; end
+
+  def destroy; end
+
+  private
+
+  def load_courses subject
+    @courses = subject.courses.paginate(page: params[:page], per_page: Settings.per_page.config)
+  end
+
+  def load_trainers_trainees subject
+    @trainers = subject.users.with_suppervisor.alphabet_name.paginate(page: params[:page],
+      per_page: Settings.per_page.config)
+    @trainees = subject.users.without_suppervisor.alphabet_name.paginate(page: params[:page],
+      per_page: Settings.per_page.config)
+  end
+
+  def load_tasks subject
+    @tasks = subject.task.alphabet_name.paginate(page: params[:page],
+      per_page: Settings.per_page.config)
+  end
+
+  def subject_params
+    params.require(:subject).permit(:name, :description)
+  end
+
+  def load_subject
+    @subject = Subject.find_by id: params[:id]
+    return if @subject
+    flash[:danger] = t "subjects.show.flash_danger"
+    redirect_to suppervisor_subjects_path
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :subject
   has_many :user_task
+  scope :alphabet_name, ->{order :name}
 end
