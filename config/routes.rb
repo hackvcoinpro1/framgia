cd68160904f3f39e5f69240163db5cdebab03f82
Alex <lisatthu35@gmail.com>
2017-10-27 14:24:36 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
 
-  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
 
   resources :users, except: [:create, :destroy, :new]
 
